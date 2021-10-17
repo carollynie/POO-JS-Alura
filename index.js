65de9b1b10bed0e5024ca3efc7d84ec36e26f960
Ana Carolina <carolina.oliveiralima9@gmail.com>
2021-10-17 16:04:15 -0300

    Atributos privados para saldo de conta

---
+++ index.js
@@ -0,0 +1,37 @@
+class cliente {
+    nome
+    cpf
+}
+
+class contaCorrente {
+    agencia
+    #saldo = 0  //o saldo é privado, e só pode ser mudado a partir da função e o valor inicial é zero
+
+    sacar(valor) {
+        if (this.#saldo >= valorSacado) { //o this deixarar dinômico a conta corrente que queremos fazer o saque
+            this.#saldo -= valorSacado //subtrai valor do saldo
+        }
+    }
+    depositar(valor){
+        if(valor>0){
+            this.#saldo+=valor
+        }
+    }
+}
+
+
+
+
+const cliente1 = new cliente() //instancia da classe
+cliente1.nome = "Carol"
+cliente1.cpf = 11122233309
+
+const cliente2 = new cliente()
+cliente2.nome = "Mikael"
+cliente2.cpf = 32022233309
+
+const contaCorrente1 = new contaCorrente()
+contaCorrente1.agencia = 1001
+
+contaCorrente1.depositar(100)
+console.log(contaCorrente1)
